1bd778d402f0c39867d90881c35b0d6dffcca75b
Shmugeliuk Oleg <shm.oleh29@gmail.com>
2026-02-11 11:28:16 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="text-3xl font-bold text-red-500">
+  Tailwind v4 працює 🔥
+</div>
+
+    </>
+  )
+}
+
+export default App
